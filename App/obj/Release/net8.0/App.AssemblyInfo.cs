5045e04286aeda74031f8492134488b35659ede0
rattanapong teeranaew <rattanapongt@singerthai.co.th>
2024-07-26 11:14:35 +0700

    UPDATE

--- App/obj/Release/net8.0/App.AssemblyInfo.cs
+++ App/obj/Release/net8.0/App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b01aafc91a83c33dc5c1abaf76497aa43293e09f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c681b9950143ae0ad00d97941a8d3231a9c32c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
